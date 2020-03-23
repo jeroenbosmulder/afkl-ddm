25878998754808e4b19b8bd6f344ed159a76f72d
Jeroen Mulder <jeroen@mulder.com>
2020-03-23 14:15:59 +0000

    Update

--- afklddm/model.py
+++ afklddm/model.py
@@ -1,13 +1,17 @@
 def run():
-    import sys
-    import argparse
+    print("Hello World")
+    
+    
+    
+#    import sys
+#    import argparse
+#
+#    parser = argparse.ArgumentParser(description='Process the \"Hello World\" message.')
+#    parser.add_argument('--hello_world_message', type=str, default="Hello World...")
+#    parser.add_argument('--input_data_dir', type=str)
+#    parser.add_argument('--working_dir', type=str)
+#    parser.add_argument('--output_data_dir', type=str)
+#    args = parser.parse_args()
 
-    parser = argparse.ArgumentParser(description='Process the \"Hello World\" message.')
-    parser.add_argument('--hello_world_message', type=str, default="Hello World...")
-    parser.add_argument('--input_data_dir', type=str)
-    parser.add_argument('--working_dir', type=str)
-    parser.add_argument('--output_data_dir', type=str)
-    args = parser.parse_args()
-
-    print(args.hello_world_message)
-    print("Argument List:"+str(sys.argv))
+#    print(args.hello_world_message)
+#    print("Argument List:"+str(sys.argv))
