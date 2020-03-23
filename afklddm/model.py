f6fb1d4603784aabc1460953989ab69fe8a008ae
Jeroen Mulder <jeroen@mulder.com>
2020-03-23 12:02:21 +0000

    Rename dummy to model

---
+++ afklddm/model.py
@@ -0,0 +1,12 @@
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='Process the \"Hello World\" message.')
+parser.add_argument('--hello_world_message', type=str, default="Hello World...")
+parser.add_argument('--input_data_dir', type=str)
+parser.add_argument('--working_dir', type=str)
+parser.add_argument('--output_data_dir', type=str)
+args = parser.parse_args()
+
+print(args.hello_world_message)
+print("Argument List:"+str(sys.argv))
